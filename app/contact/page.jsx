dfc6d9336d4dd7985ed4e13012b8f01c7bf500f6
Muhammad zeeshan <fullstackdeveloper486@gmail.com>
2024-09-26 21:27:12 +0500

    Navigating Programmatically

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,9 +1,23 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
-export const metadata = {
-  title: "Contact",
-};
+// export const metadata = {
+//   title: "Contact",
+// };
 const Contact = () => {
-  return <div>Contact</div>;
+  const router = useRouter();
+  console.log(router);
+  const handleclick = () => {
+    // sucessfull swal fire after that
+    // redirect to home page
+    router.push("/");
+  };
+  return (
+    <div>
+      Contact
+      <button onClick={handleclick}>submit</button>
+    </div>
+  );
 };
 
 export default Contact;
